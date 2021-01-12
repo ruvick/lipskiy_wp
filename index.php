69f30d8d11c815d468c6c55e0d11f0e0487f3329
ruvick <rudikov.web@ya.ru>
2021-01-12 20:10:39 +0300

    Обернул в ссылки блоки услуг

--- index.php
+++ index.php
@@ -78,52 +78,53 @@
 				</h2>
 			</div>
 
-			<div class="services__item services__img-01" style="background-image: url(<?php echo get_template_directory_uri();?>/img/services/01.jpg);">
+			<a href="<?php echo get_permalink(31);?>" class="services__item services__img-01" style="background-image: url(<?php echo get_template_directory_uri();?>/img/services/01.jpg);">
 				<div class="services__item-text">
 					<p>
 						Стратегический и
 						инвестиционный
 						консалтинг
 					</p>
-				</div>"
-			</div>
+				</div>
+			</a>
 
-			<div class="services__item services__img-02" style="background-image: url(<?php echo get_template_directory_uri();?>/img/services/02.jpg);">
+
+			<a href="<?php echo get_permalink(20);?>" class="services__item services__img-02" style="background-image: url(<?php echo get_template_directory_uri();?>/img/services/02.jpg);">
 				<div class="services__item-text">
 					<p>
 						Управленческий 
 						консалтинг
 					</p>
-				</div>"
-			</div>
+				</div>
+			</a>
 
-			<div class="services__item services__img-03" style="background-image: url(<?php echo get_template_directory_uri();?>/img/services/03.jpg);">
+			<a href="<?php echo get_permalink(33);?>" class="services__item services__img-03" style="background-image: url(<?php echo get_template_directory_uri();?>/img/services/03.jpg);">
 				<div class="services__item-text">
 					<p>
 						Налоговый и 
 						бухгалтерский
 						консалтинг 
 					</p>
-				</div>"
-			</div>
+				</div>
+			</a>
 
-			<div class="services__item services__img-04" style="background-image: url(<?php echo get_template_directory_uri();?>/img/services/04.jpg);">
+			<a href="<?php echo get_permalink(35);?>" class="services__item services__img-04" style="background-image: url(<?php echo get_template_directory_uri();?>/img/services/04.jpg);">
 				<div class="services__item-text">
 					<p>
 						Правовой 
 						консалтинг
 					</p>
-				</div>"
-			</div>
+				</div>
+			</a>
 
-			<div class="services__item services__img-05" style="background-image: url(<?php echo get_template_directory_uri();?>/img/services/05.jpg);">
+			<a href="<?php echo get_permalink(37);?>" class="services__item services__img-05" style="background-image: url(<?php echo get_template_directory_uri();?>/img/services/05.jpg);">
 				<div class="services__item-text">
 					<p>
 						Маркетинговый 
 						консалтинг 
 					</p>
-				</div>"
-			</div>
+				</div>
+			</a>
 
 		</div>
 	</div>
