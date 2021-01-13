803d663064fb388407288c91ee79fc0accb1f3f7
ruvick <rudikov.web@ya.ru>
2021-01-13 12:39:05 +0300

    Добавил типовую шапку

--- page-contacts.php
+++ page-contacts.php
@@ -7,14 +7,14 @@ Template Post Type: page
 
 get_header(); ?>
 
-<?php get_template_part('template-parts/header-section');?>
+<?php get_template_part('template-parts/header-section-tip');?>
 
 <section class="contacts">
 	<div class="container">
-		<h1><?php the_title();?></h1>
+
 	</div>
 	<div class="block__map" id="map"></div>
-		<script src="http://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+	<script src="http://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
 </section>
 
 <?php get_footer(); ?>  
