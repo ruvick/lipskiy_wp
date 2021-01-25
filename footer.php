9fe5aa09dd185c838c4b54d542c1b259614aeef7
ruvick <rudikov.web@ya.ru>
2021-01-25 10:17:52 +0300

    Подключение соцсетей в футере из админки

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
           <a href="http://lipskiy-konsalting.ru" class="header__logo footer__logo icon-logo-f"><p>ЛИПСКИЙ <br> <span>И ПАРТНЕРЫ</span></p></a>
           <div class="footer__contact d-flex">
             <div class="footer__social d-flex">
-              <a href="#" aria-label="instagram" class="footer__icon icon-insta"></a>
-              <a href="#" aria-label="facebook" class="footer__icon icon-face"></a>
-              <a href="#" aria-label="ВКонтакте" class="footer__icon icon-vk"></a>
+              <a href="<? echo carbon_get_theme_option("as_insta"); ?>" aria-label="instagram" class="footer__icon icon-insta"></a>
+              <a href="<? echo carbon_get_theme_option("as_face"); ?>" aria-label="facebook" class="footer__icon icon-face"></a>
+              <a href="<? echo carbon_get_theme_option("as_vk"); ?>" aria-label="ВКонтакте" class="footer__icon icon-vk"></a>
             </div>
             <a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="footer__tel"><? echo $tel = carbon_get_theme_option("as_phones_1"); ?></a> 
           </div>
