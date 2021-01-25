7bc6669ad5eaea0b9d927045edd7c833217d647b
ruvick <rudikov.web@ya.ru>
2021-01-25 10:04:47 +0300

    Подключил мобильную шапку в Услугах

--- template-parts/header-section-services.php
+++ template-parts/header-section-services.php
@@ -7,41 +7,42 @@ if(empty($banner)) {
 
 ?>
 
+<!-- Шапка ПК -->
 <header class="header-services header">
-		<div class="header__img">
-			<div class="header__img-big header-services__img-big">
-				<div class="header-services__img" style="background-image: url(<?php echo $banner?>);"> 
+	<div class="header__img">
+		<div class="header__img-big header-services__img-big">
+			<div class="header-services__img" style="background-image: url(<?php echo $banner?>);"> 
 
-				</div>
-				<!-- <img src="<?php echo $banner?>" alt=""> -->
 			</div>
-			<div class="header__block-grey"></div>
-			<div class="header__block-green-small"></div>
-			<div class="header__block-green"></div> 
+			<!-- <img src="<?php echo $banner?>" alt=""> -->
 		</div>
+		<div class="header__block-grey"></div>
+		<div class="header__block-green-small"></div>
+		<div class="header__block-green"></div> 
+	</div>
 
-		<div class="header__container container header-services__container">
+	<div class="header__container container header-services__container">
 
-			<div class="header__row header-services__row d-flex">
-				<a href="http://lipskiy-konsalting.ru" class="header__logo icon-logo"><p>ЛИПСКИЙ <br> <span>И ПАРТНЕРЫ</span></p></a>
-				<div class="header__menu menu d-flex">
-					<div class="menu__icon icon-menu"> 
+		<div class="header__row header-services__row d-flex">
+			<a href="http://lipskiy-konsalting.ru" class="header__logo icon-logo"><p>ЛИПСКИЙ <br> <span>И ПАРТНЕРЫ</span></p></a>
+			<div class="header__menu menu d-flex">
+<!-- 					<div class="menu__icon icon-menu"> 
 						<span></span>
 						<span></span>
 						<span></span>
-					</div>
+					</div> -->
 					<nav class="menu__body">
 						<?php wp_nav_menu( array('theme_location' => 'menu-1','menu_class' => 'menu__list',
 						'container_class' => 'menu__list','container' => false )); ?>
-				</nav>
-				<nav class="mob-menu">
+					</nav>
+<!-- 				<nav class="mob-menu">
 					<?php wp_nav_menu( array('theme_location' => 'menu-2','menu_class' => 'menu__list',
 					'container_class' => 'menu__list','container' => false )); ?>
 					<div class="mobile__contact d-flex">
 						<a href="mailto:<? echo $mail = carbon_get_theme_option("as_email"); ?>" class="mobile-mail contact-mail"><? echo $mail; ?></a>
 						<a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="mobile-tel contact-tel"><? echo $tel = carbon_get_theme_option("as_phones_1"); ?></a>
 					</div>
-				</nav>
+				</nav> -->
 				<div class="header__contact d-flex">
 					<a href="mailto:<? echo $mail = carbon_get_theme_option("as_email"); ?>" class="mobile-mail contact-mail"><? echo $mail; ?></a>
 					<a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="contact-tel"><? echo $tel = carbon_get_theme_option("as_phones_1"); ?></a>
@@ -71,7 +72,7 @@ if(empty($banner)) {
 
 </header>  
 
-
+<!-- Шапка Mobile -->
 <header class="header-services header-services-bg header" style="background-image: url(<?php echo $banner?>);">
 <!-- 		<div class="header__img">
 			<div class="header__img-big header-services__img-big">
@@ -98,40 +99,40 @@ if(empty($banner)) {
 					<nav class="menu__body">
 						<?php wp_nav_menu( array('theme_location' => 'menu-1','menu_class' => 'menu__list',
 						'container_class' => 'menu__list','container' => false )); ?>
-				</nav>
-				<nav class="mob-menu">
-					<?php wp_nav_menu( array('theme_location' => 'menu-2','menu_class' => 'menu__list',
-					'container_class' => 'menu__list','container' => false )); ?>
-					<div class="mobile__contact d-flex">
+					</nav>
+					<nav class="mob-menu">
+						<?php wp_nav_menu( array('theme_location' => 'menu-2','menu_class' => 'menu__list',
+						'container_class' => 'menu__list','container' => false )); ?>
+						<div class="mobile__contact d-flex">
+							<a href="mailto:<? echo $mail = carbon_get_theme_option("as_email"); ?>" class="mobile-mail contact-mail"><? echo $mail; ?></a>
+							<a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="mobile-tel contact-tel"><? echo $tel = carbon_get_theme_option("as_phones_1"); ?></a>
+						</div>
+					</nav>
+					<div class="header__contact d-flex">
 						<a href="mailto:<? echo $mail = carbon_get_theme_option("as_email"); ?>" class="mobile-mail contact-mail"><? echo $mail; ?></a>
-						<a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="mobile-tel contact-tel"><? echo $tel = carbon_get_theme_option("as_phones_1"); ?></a>
+						<a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="contact-tel"><? echo $tel = carbon_get_theme_option("as_phones_1"); ?></a>
 					</div>
-				</nav>
-				<div class="header__contact d-flex">
-					<a href="mailto:<? echo $mail = carbon_get_theme_option("as_email"); ?>" class="mobile-mail contact-mail"><? echo $mail; ?></a>
-					<a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="contact-tel"><? echo $tel = carbon_get_theme_option("as_phones_1"); ?></a>
 				</div>
 			</div>
+
+			<div class="header-copy__breadcrumb">
+				<?php
+				if (function_exists ('yoast_breadcrumb')) {
+					yoast_breadcrumb ('<p id = "breadcrumbs">', '</p>');
+				}
+				?>
+			</div>
+
+			<h1 class="header-services__title">
+				<?php the_title();?>
+			</h1>
+
+			<div class="header-services-sub">
+				<?php the_excerpt(); ?>
+			</div>
+
+			<a href="#callback" class="btn link pl">Жду звонка</a>
+
 		</div>
 
-		<div class="header-copy__breadcrumb">
-			<?php
-			if (function_exists ('yoast_breadcrumb')) {
-				yoast_breadcrumb ('<p id = "breadcrumbs">', '</p>');
-			}
-			?>
-		</div>
-
-		<h1 class="header-services__title">
-			<?php the_title();?>
-		</h1>
-
-		<div class="header-services-sub">
-			<?php the_excerpt(); ?>
-		</div>
-
-		<a href="#callback" class="btn link pl">Жду звонка</a>
-
-	</div>
-
-</header>  
+	</header>  
