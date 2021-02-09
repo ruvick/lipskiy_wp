2120f9add85b935c3d46764851e207112942f2c0
ruvick <rudikov.web@ya.ru>
2021-02-09 12:32:09 +0300

    Вставил мета тег

--- header.php
+++ header.php
@@ -27,6 +27,7 @@
   <link rel="icon" type="image/svg+xml" sizes="any" href="<? echo get_template_directory_uri();?>/image/favicons/icon-faw.svg">
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+  <meta name="yandex-verification" content="5f190a4ac67e56fb" />
   
   <?php wp_head();?> 
   
