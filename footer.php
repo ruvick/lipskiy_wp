16ccc159b19dc1908490c5b66fcee7ec01cd8be4
asmi046 <asmi046@gmail.com>
2022-03-22 13:44:27 +0300

    -insta

--- footer.php
+++ footer.php
@@ -5,8 +5,6 @@
           <div class="footer__contact d-flex">
             <div class="footer__social d-flex">
               <a href="<? echo carbon_get_theme_option("as_telegr"); ?>" aria-label="telegram" class="footer__icon icon-telegr"></a>
-              <a href="<? echo carbon_get_theme_option("as_insta"); ?>" aria-label="instagram" class="footer__icon icon-insta"></a>
-              <a href="<? echo carbon_get_theme_option("as_face"); ?>" aria-label="facebook" class="footer__icon icon-face"></a>
               <a href="<? echo carbon_get_theme_option("as_vk"); ?>" aria-label="ВКонтакте" class="footer__icon icon-vk"></a>
             </div>
             <a href="tel:<? echo preg_replace('/[^0-9]/', '', $tel); ?>" class="footer__tel"><? echo $tel = carbon_get_theme_option("as_phones_1"); ?></a> 
